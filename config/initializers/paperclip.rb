5f531298bb0c585a58d41bdf2e9cbe5a8c387df0
Nick Peters <freon27@gmail.com>
2014-12-16 09:39:46 +0000

    testing url link to s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,1 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
-
-
-Paperclip.interpolates(:s3_eu_url) { |attachment, style|
-  "#{attachment.s3_protocol}://s3-eu-west-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
-}
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
